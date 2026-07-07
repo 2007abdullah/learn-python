ec745400b2f94a0eb8f073c9ea0421e1e819fb0d
2007abdullah <abdullahhyaat@gmail.com>
2026-07-07 13:38:24 -0700

    Complete Day-1

---
+++ Day-1/student.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+age = input("Enter your age: ")
+print("You are " + age + " years old.")
+
+course = input("Enter your course: ")
+print("You are enrolled in the " + course + " course.")
+
+print("Thank you for providing your information, " + name + ". Have a great day!")
+print("You are " + age + " years old and enrolled in the " + course + " course.")
