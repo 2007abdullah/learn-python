61b173e6b4a05a7e0c222942c43ffc785fd66741
2007abdullah <abdullahhyaat@gmail.com>
2026-07-11 10:59:09 -0700

    Updated

---
+++ Day-11/bank.py
@@ -0,0 +1,22 @@
+class bankaccount:
+    def __init__(self,name,balance):
+        self.name=name
+        self.balance=balance
+
+    def deposit(self,amount):
+            self.balance+=amount
+            print("Amount deposited:",amount)
+            print("New balance:",self.balance)
+
+    def withdraw(self,amount):
+        if amount>self.balance:
+            print("Insufficient balance")
+        else:
+            self.balance-=amount
+            print("Amount withdrawn:",amount)
+            print("New balance:",self.balance)
+
+
+user1= bankaccount("Abdullah",1000)
+user1.deposit(500)
+user1.withdraw(200)
