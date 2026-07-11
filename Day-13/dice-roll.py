f28a9664c41d0cf70d81bfbb061345b12d8f22df
2007abdullah <abdullahhyaat@gmail.com>
2026-07-11 12:31:15 -0700

    Updated project

---
+++ Day-13/dice-roll.py
@@ -0,0 +1,10 @@
+import random
+
+while True:
+    input("Press Enter to roll dice...")
+    print("You got:", random.randint(1, 6))
+
+    break_choice = input("Roll again? (y/n): ")
+    if break_choice.lower() != 'y':
+        break
+     
